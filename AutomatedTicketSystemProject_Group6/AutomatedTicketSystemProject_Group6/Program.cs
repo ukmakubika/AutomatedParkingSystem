0f9c6b40dc8a4cae851d0debfb9f76b692d9b43f
ukmakubika <ukmakubika@gmail.com>
2022-09-01 20:29:12 +0200

    Restart

--- AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/Program.cs
+++ AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace AutomatedParkingSystem_Group6
+namespace AutomatedTicketSystemProject_Group6
 {
     internal static class Program
     {
@@ -16,7 +16,7 @@ namespace AutomatedParkingSystem_Group6
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainLogin());
+            Application.Run(new Form1());
         }
     }
 }
--- AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/Properties/Resources.Designer.cs
+++ AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AutomatedParkingSystem_Group6.Properties
+namespace AutomatedTicketSystemProject_Group6.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace AutomatedParkingSystem_Group6.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AutomatedParkingSystem_Group6.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AutomatedTicketSystemProject_Group6.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
