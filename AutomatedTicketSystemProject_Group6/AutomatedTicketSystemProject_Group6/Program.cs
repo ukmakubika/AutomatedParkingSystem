8f8e4dbd84b77cd9054135b9f06903d6f4a20893
ukmakubika <ukmakubika@gmail.com>
2022-09-01 20:45:43 +0200

    GUI Update

--- AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/Program.cs
+++ AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/Program.cs
@@ -16,7 +16,7 @@ namespace AutomatedTicketSystemProject_Group6
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainLogin());
         }
     }
 }
