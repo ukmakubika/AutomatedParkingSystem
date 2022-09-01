dcba4ca196abc274607386ad56d3ab31d8cf1784
ukmakubika <ukmakubika@gmail.com>
2022-09-01 20:49:48 +0200

    Changes to Main

--- AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/Form1.Designer.cs
+++ AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AutomatedTicketSystemProject_Group6
 {
-    partial class Form1
+    partial class MainLogin
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pbManager = new System.Windows.Forms.PictureBox();
+            this.pbClient = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.linkPassword = new System.Windows.Forms.LinkLabel();
+            this.linkSignup = new System.Windows.Forms.LinkLabel();
+            this.rbManager = new System.Windows.Forms.RadioButton();
+            this.rbClient = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pbManager)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbClient)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbManager
+            // 
+            this.pbManager.Location = new System.Drawing.Point(39, 33);
+            this.pbManager.Name = "pbManager";
+            this.pbManager.Size = new System.Drawing.Size(190, 188);
+            this.pbManager.TabIndex = 0;
+            this.pbManager.TabStop = false;
+            // 
+            // pbClient
+            // 
+            this.pbClient.Location = new System.Drawing.Point(340, 33);
+            this.pbClient.Name = "pbClient";
+            this.pbClient.Size = new System.Drawing.Size(190, 188);
+            this.pbClient.TabIndex = 1;
+            this.pbClient.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 312);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 360);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(39, 405);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(90, 34);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(126, 305);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(193, 20);
+            this.txtUsername.TabIndex = 5;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(126, 353);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(193, 20);
+            this.txtPassword.TabIndex = 6;
+            // 
+            // linkPassword
+            // 
+            this.linkPassword.AutoSize = true;
+            this.linkPassword.Location = new System.Drawing.Point(337, 360);
+            this.linkPassword.Name = "linkPassword";
+            this.linkPassword.Size = new System.Drawing.Size(92, 13);
+            this.linkPassword.TabIndex = 7;
+            this.linkPassword.TabStop = true;
+            this.linkPassword.Text = "Forgot Password?";
+            // 
+            // linkSignup
+            // 
+            this.linkSignup.AutoSize = true;
+            this.linkSignup.Location = new System.Drawing.Point(337, 312);
+            this.linkSignup.Name = "linkSignup";
+            this.linkSignup.Size = new System.Drawing.Size(111, 13);
+            this.linkSignup.TabIndex = 8;
+            this.linkSignup.TabStop = true;
+            this.linkSignup.Text = "No Account? Sign Up";
+            // 
+            // rbManager
+            // 
+            this.rbManager.AutoSize = true;
+            this.rbManager.Location = new System.Drawing.Point(39, 260);
+            this.rbManager.Name = "rbManager";
+            this.rbManager.Size = new System.Drawing.Size(67, 17);
+            this.rbManager.TabIndex = 9;
+            this.rbManager.TabStop = true;
+            this.rbManager.Text = "Manager";
+            this.rbManager.UseVisualStyleBackColor = true;
+            // 
+            // rbClient
+            // 
+            this.rbClient.AutoSize = true;
+            this.rbClient.Location = new System.Drawing.Point(340, 260);
+            this.rbClient.Name = "rbClient";
+            this.rbClient.Size = new System.Drawing.Size(51, 17);
+            this.rbClient.TabIndex = 10;
+            this.rbClient.TabStop = true;
+            this.rbClient.Text = "Client";
+            this.rbClient.UseVisualStyleBackColor = true;
+            // 
+            // MainLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(569, 477);
+            this.Controls.Add(this.rbClient);
+            this.Controls.Add(this.rbManager);
+            this.Controls.Add(this.linkSignup);
+            this.Controls.Add(this.linkPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbClient);
+            this.Controls.Add(this.pbManager);
+            this.Name = "MainLogin";
+            this.Text = "Main Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pbManager)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbClient)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbManager;
+        private System.Windows.Forms.PictureBox pbClient;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.LinkLabel linkPassword;
+        private System.Windows.Forms.LinkLabel linkSignup;
+        private System.Windows.Forms.RadioButton rbManager;
+        private System.Windows.Forms.RadioButton rbClient;
     }
 }
 
--- AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/Form1.cs
+++ AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AutomatedTicketSystemProject_Group6
 {
-    public partial class Form1 : Form
+    public partial class MainLogin : Form
     {
-        public Form1()
+        public MainLogin()
         {
             InitializeComponent();
         }
