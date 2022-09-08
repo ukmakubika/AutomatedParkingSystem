84364f555f3dbcd94b165c1b5b7736e341af75c3
lexer404 <ompabaletsevece@gmail.com>
2022-09-08 18:24:19 +0200

    lexer404 - First Changes

--- AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/Login.Designer.cs
+++ AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/Login.Designer.cs
@@ -87,6 +87,7 @@
             this.btnLogin.TabIndex = 4;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // txtUsername
             // 
--- AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/Login.cs
+++ AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/Login.cs
@@ -16,5 +16,10 @@ namespace AutomatedTicketSystemProject_Group6
         {
             InitializeComponent();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
