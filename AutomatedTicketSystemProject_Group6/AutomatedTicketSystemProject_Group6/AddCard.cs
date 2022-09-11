33566595fc2ed058e936453bb56df5390f0d4dc9
ukmakubika <ukmakubika@gmail.com>
2022-09-11 18:30:17 +0200

    Updates
    -Add Card Form
    -New Customer GUI Payment Detail look: Add button, Delete Button, and Show all Button
    - Gridview
    -Incomplete insert code (Bank)
    - complete delete code (Bank)

--- AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/AddCard.cs
+++ AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/AddCard.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace AutomatedTicketSystemProject_Group6
 {
@@ -15,6 +16,92 @@ namespace AutomatedTicketSystemProject_Group6
         public AddCard()
         {
             InitializeComponent();
+        }
+
+        //Connection of data
+        SqlConnection connection;
+        string cnctString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TicketSystem;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        //Retrieving data
+        SqlDataAdapter adapter;
+        SqlCommand command;
+        DataSet data;
+
+        public string name;
+        
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            float cardNo;
+            int CVV;
+            if (float.TryParse(txtCardNo.Text, out cardNo))
+            {
+                if (txtCardNo.Text.Length != 16)
+                {
+                    MessageBox.Show("Please enter a valid length card number.");
+                }
+                else
+                {
+                    if (int.TryParse(txtCVV.Text, out CVV))
+                    {
+                        if (txtCVV.Text.Length != 3)
+                        {
+                            MessageBox.Show("Please enter a valid length CVV number.");
+                        }
+                        else
+                        {
+                            if (cbBankNames.SelectedItem == null)
+                            {
+                                MessageBox.Show("Please select a bank");
+                            }
+                            if (cbMonth.SelectedItem == null)
+                            {
+                                MessageBox.Show("Please select a month");
+                            }
+                            if (cbYear.SelectedItem == null)
+                            {
+                                MessageBox.Show("Please select a year");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a valid cvv code");
+                    }
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid card number");
+            }
+
+            txtExpDate.Text = cbMonth.Text + " " + cbYear.Text;
+            try
+            {
+                name = txtCardholderName.Text;
+
+                string addme = $"INSERT INTO BANK(Bank_name, Exp_date Card_no, Card_HolderName) VALUES('{cbBankNames.Text}', '{txtCardNo.Text}', '{txtExpDate.Text}','{txtCardholderName.Text}')";//tell to insert in string language
+
+                connection = new SqlConnection(cnctString);//whats it gonna connect to 
+
+                connection.Open(); //open connection
+
+                command = new SqlCommand(addme, connection); //tell it what r=to do
+                adapter = new SqlDataAdapter(); //populate data table
+                adapter.InsertCommand = command; //tel it to insert
+                adapter.InsertCommand.ExecuteNonQuery();
+
+                
+                connection.Close(); // close connection
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+
         }
     }
 }
--- AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/CustomerInterface.Designer.cs
+++ AutomatedTicketSystemProject_Group6/AutomatedTicketSystemProject_Group6/CustomerInterface.Designer.cs
@@ -49,9 +49,16 @@
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.dbBankAccounts = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtDelete = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDisplay = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.MainPage.SuspendLayout();
             this.groupBox4.SuspendLayout();
@@ -60,7 +67,7 @@
             this.PaymentDetails.SuspendLayout();
             this.Help.SuspendLayout();
             this.groupBox5.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbBankAccounts)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -201,9 +208,16 @@
             // 
             // PaymentDetails
             // 
-            this.PaymentDetails.Controls.Add(this.button2);
-            this.PaymentDetails.Controls.Add(this.button1);
-            this.PaymentDetails.Controls.Add(this.dataGridView1);
+            this.PaymentDetails.Controls.Add(this.txtDisplay);
+            this.PaymentDetails.Controls.Add(this.label6);
+            this.PaymentDetails.Controls.Add(this.txtDelete);
+            this.PaymentDetails.Controls.Add(this.lblName);
+            this.PaymentDetails.Controls.Add(this.label4);
+            this.PaymentDetails.Controls.Add(this.txtSearch);
+            this.PaymentDetails.Controls.Add(this.label1);
+            this.PaymentDetails.Controls.Add(this.btnDelete);
+            this.PaymentDetails.Controls.Add(this.btnAdd);
+            this.PaymentDetails.Controls.Add(this.dbBankAccounts);
             this.PaymentDetails.Location = new System.Drawing.Point(4, 22);
             this.PaymentDetails.Name = "PaymentDetails";
             this.PaymentDetails.Padding = new System.Windows.Forms.Padding(3);
@@ -211,6 +225,7 @@
             this.PaymentDetails.TabIndex = 1;
             this.PaymentDetails.Text = "Payment Details";
             this.PaymentDetails.UseVisualStyleBackColor = true;
+            this.PaymentDetails.Click += new System.EventHandler(this.PaymentDetails_Click);
             // 
             // Help
             // 
@@ -290,31 +305,97 @@
             this.label13.TabIndex = 11;
             this.label13.Text = "Whether you’re a manager or customer, we’re here to offer a helping hand. ";
             // 
-            // dataGridView1
+            // dbBankAccounts
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(324, 30);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(420, 197);
-            this.dataGridView1.TabIndex = 0;
+            this.dbBankAccounts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dbBankAccounts.Location = new System.Drawing.Point(299, 80);
+            this.dbBankAccounts.Name = "dbBankAccounts";
+            this.dbBankAccounts.Size = new System.Drawing.Size(434, 290);
+            this.dbBankAccounts.TabIndex = 0;
             // 
-            // button1
+            // btnAdd
             // 
-            this.button1.Location = new System.Drawing.Point(31, 30);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(84, 37);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAdd.Location = new System.Drawing.Point(35, 212);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(104, 36);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
-            // button2
+            // btnDelete
             // 
-            this.button2.Location = new System.Drawing.Point(31, 100);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(84, 37);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnDelete.Location = new System.Drawing.Point(33, 276);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(104, 36);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Search";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(35, 108);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(205, 20);
+            this.txtSearch.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(312, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "What would you like to do today,";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(337, 31);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(65, 24);
+            this.lblName.TabIndex = 6;
+            this.lblName.Text = "Name";
+            // 
+            // txtDelete
+            // 
+            this.txtDelete.Location = new System.Drawing.Point(33, 349);
+            this.txtDelete.Name = "txtDelete";
+            this.txtDelete.Size = new System.Drawing.Size(205, 20);
+            this.txtDelete.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(31, 330);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Card to Delete";
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(34, 153);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(104, 36);
+            this.txtDisplay.TabIndex = 9;
+            this.txtDisplay.Text = "Show All";
+            this.txtDisplay.UseVisualStyleBackColor = true;
+            this.txtDisplay.Click += new System.EventHandler(this.txtDisplay_Click);
             // 
             // CustomerInterface
             // 
@@ -324,6 +405,7 @@
             this.Controls.Add(this.tabControl1);
             this.Name = "CustomerInterface";
             this.Text = "Functions";
+            this.Load += new System.EventHandler(this.CustomerInterface_Load);
             this.tabControl1.ResumeLayout(false);
             this.MainPage.ResumeLayout(false);
             this.MainPage.PerformLayout();
@@ -333,10 +415,11 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.PaymentDetails.ResumeLayout(false);
+            this.PaymentDetails.PerformLayout();
             this.Help.ResumeLayout(false);
             this.Help.PerformLayout();
             this.groupBox5.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbBankAccounts)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -364,8 +447,15 @@
         private System.Windows.Forms.ComboBox cbFAQ;
         private System.Windows.Forms.Label label14;
         private System.Windows.Forms.ListBox listAnswer;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dbBankAccounts;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDelete;
+        private System.Windows.Forms.Button txtDisplay;
     }
 }
